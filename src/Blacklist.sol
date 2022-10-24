b9af44405ea6282e2ca97346ef65deb8d331e151
NutiNaguti <derzahvin.danila@icloud.com>
2022-10-24 17:45:37 +0400

    added old contracts and readme

---
+++ src/Blacklist.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./interfaces/IBlacklist.sol";
+
+contract Blacklist is Ownable, IBlacklist {
+    mapping(address => bool) _blacklisted;
+
+    function addToBlacklist(address user) external onlyOwner {
+        require(!_blacklisted[user], "Blacklist: user already blacklisted");
+        _blacklisted[user] = true;
+    }
+
+    function removeFromBlacklist(address user) external onlyOwner {
+        require(_blacklisted[user], "Blacklist: user not in blacklist");
+        _blacklisted[user] = false;
+    }
+
+    function isInBlacklist(address user) external view returns (bool) {
+        return _blacklisted[user];
+    }
+}
---
+++ src/interfaces/IAssets.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+
+interface IAssets is IERC721Upgradeable {
+    struct Asset {
+        string content;
+    }
+
+    function lockToken(uint256 tokenId) external;
+
+    function unlockToken(uint256 tokenId) external;
+
+    function isLocked(uint256 tokenId) external view returns (bool);
+}
